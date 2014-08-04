4d64d4c8f6de7e852ad9a46070cce10b8ed1a41d
Federico Gandellini <fgandellini@gmail.com>
2014-08-04 23:53:32 +0200

    map extraction function

---
+++ test/ftjs.js
@@ -0,0 +1,356 @@
+'use strict';
+
+var expect = require('chai').expect;
+var ftjs = require('../lib/ftjs');
+
+describe('ftjs', function() {
+
+  it('should exist', function() {
+    expect(ftjs).to.exist;
+  });
+
+  it('should be a nodejs module', function() {
+    expect(ftjs).to.be.a('function');
+  });
+
+  xit('should take pdf form as parameter', function() {
+
+  });
+
+  xit('should take a map object', function() {
+
+  });
+
+  describe('extractMaps', function() {
+
+    it('should be a function', function() {
+      expect(ftjs().extractMaps).to.be.a('function');
+    });
+
+    it('should skip empty object', function(done) {
+      var map = {};
+      ftjs().extractMaps(map, function(data) {
+        expect(data).to.deep.equal({});
+        done();
+      });
+    });
+
+    it('should skip generic object', function(done) {
+      var map = {
+        str: 'string',
+        num: 2,
+        fn: function() {}
+      };
+      ftjs().extractMaps(map, function(data) {
+        expect(data.str).to.equal('string');
+        expect(data.num).to.equal(2);
+        expect(data.fn).to.be.a('function');
+        done();
+      });
+    });
+
+    it('should skip empty array', function(done) {
+      var map = [];
+      ftjs().extractMaps(map, function(data) {
+        expect(data).to.deep.equal([]);
+        done();
+      });
+    });
+
+    it('should skip generic array', function(done) {
+      var map = [1, 2, 3];
+      ftjs().extractMaps(map, function(data) {
+        expect(data[0]).to.equal(1);
+        expect(data[1]).to.equal(2);
+        expect(data[2]).to.equal(3);
+        done();
+      });
+    });
+
+    it('should skip number', function(done) {
+      var map = 1;
+      ftjs().extractMaps(map, function(data) {
+        expect(data).to.equal(map);
+        done();
+      });
+    });
+
+    it('should skip string', function(done) {
+      var map = 'map';
+      ftjs().extractMaps(map, function(data) {
+        expect(data).to.equal(map);
+        done();
+      });
+    });
+
+    it('should skip nested empty object', function(done) {
+      var map = {
+        nested: {}
+      };
+      ftjs().extractMaps(map, function(data) {
+        expect(data).to.deep.equal({
+          nested: {}
+        });
+        done();
+      });
+    });
+
+    it('should skip nested generic object', function(done) {
+      var map = {
+        nested: {
+          str: 'string',
+          num: 2,
+          fn: function() {}
+        }
+      };
+      ftjs().extractMaps(map, function(data) {
+        expect(data.nested.str).to.equal('string');
+        expect(data.nested.num).to.equal(2);
+        expect(data.nested.fn).to.be.a('function')
+        done();
+      });
+    });
+
+    it('should skip nested empty array', function(done) {
+      var map = {
+        nested: []
+      };
+      ftjs().extractMaps(map, function(data) {
+        expect(data).to.deep.equal({
+          nested: []
+        });
+        done();
+      });
+    });
+
+    it('should skip nested generic array', function(done) {
+      var map = {
+        nested: [1, 2, 3]
+      };
+      ftjs().extractMaps(map, function(data) {
+        expect(data).to.deep.equal({
+          nested: [1, 2, 3]
+        });
+        done();
+      });
+    });
+
+    it('should skip nested number', function(done) {
+      var map = {
+        nested: 1
+      };
+      ftjs().extractMaps(map, function(data) {
+        expect(data).to.deep.equal({
+          nested: 1
+        });
+        done();
+      });
+    });
+
+    it('should skip nested string', function(done) {
+      var map = {
+        nested: 'map'
+      };
+      ftjs().extractMaps(map, function(data) {
+        expect(data).to.deep.equal({
+          nested: 'map'
+        });
+        done();
+      });
+    });
+
+    it('should get map object', function(done) {
+      var map = {
+        x: 0,
+        y: 0,
+        h: 0,
+        w: 0
+      };
+      ftjs().extractMaps(map, function(data) {
+        expect(data).to.equal('');
+        done();
+      });
+    });
+
+    it('should get map object with shuffled params', function(done) {
+      var map = {
+        h: 0,
+        x: 0,
+        w: 0,
+        y: 0
+      };
+      ftjs().extractMaps(map, function(data) {
+        expect(data).to.equal('');
+        done();
+      });
+    });
+
+    it('should skip fake map object', function(done) {
+      var map = {
+        x: 0,
+        y: 0,
+        h: 0,
+        w: 0,
+        k: 0
+      };
+      ftjs().extractMaps(map, function(data) {
+        expect(data).to.deep.equal({
+          x: 0,
+          y: 0,
+          h: 0,
+          w: 0,
+          k: 0
+        });
+        done();
+      });
+    });
+
+    it('should skip array map', function(done) {
+      var map = ['x', 'y', 'h', 'w'];
+      ftjs().extractMaps(map, function(data) {
+        expect(data).to.deep.equal(['x', 'y', 'h', 'w']);
+        done();
+      });
+    });
+
+    it('should get nested map object', function(done) {
+      var map = {
+        nested: {
+          x: 0,
+          y: 0,
+          h: 0,
+          w: 0
+        }
+      };
+      ftjs().extractMaps(map, function(data) {
+        expect(data).to.deep.equal({
+          nested: ''
+        });
+        done();
+      });
+    });
+
+    it('should get nested map object with shuffled params', function(done) {
+      var map = {
+        nested: {
+          h: 0,
+          x: 0,
+          w: 0,
+          y: 0
+        }
+      };
+      ftjs().extractMaps(map, function(data) {
+        expect(data).to.deep.equal({
+          nested: ''
+        });
+        done();
+      });
+    });
+
+    it('should skip nested fake map object', function(done) {
+      var map = {
+        nested: {
+          x: 0,
+          y: 0,
+          h: 0,
+          w: 0,
+          k: 0
+        }
+      };
+      ftjs().extractMaps(map, function(data) {
+        expect(data).to.deep.equal({
+          nested: {
+            x: 0,
+            y: 0,
+            h: 0,
+            w: 0,
+            k: 0
+          }
+        });
+        done();
+      });
+    });
+
+    it('should skip nested array map', function(done) {
+      var map = {
+        nested: ['x', 'y', 'h', 'w']
+      };
+      ftjs().extractMaps(map, function(data) {;
+        expect(data).to.deep.equal({
+          nested: ['x', 'y', 'h', 'w']
+        });
+        done();
+      });
+    });
+
+    it('should parse complex map', function(done) {
+      var map = {
+        b: 1,
+        c: 'hello',
+        d: {
+          e: 3,
+          f: 'hello'
+        },
+        g: {
+          x: 0,
+          y: 0,
+          h: 0,
+          w: 0
+        },
+        h: [1, 2, 3, 4],
+        i: [{
+          x: 0,
+          y: 0,
+          h: 0,
+          w: 0
+        }, 'hello', 1],
+        l: {
+          x: 0,
+          y: 0,
+          h: 0,
+          w: 0,
+          k: 0
+        },
+        m: {
+          x: 0,
+          h: 0,
+          y: 0,
+          w: 0
+        },
+        fun: function() {}
+      };
+      ftjs().extractMaps(map, function(data) {
+        expect(data.b).to.equal(1);
+        expect(data.c).to.equal('hello');
+        expect(data.d.e).to.equal(3);
+        expect(data.d.f).to.equal('hello');
+        expect(data.g).to.equal('');
+        expect(data.h[0]).to.equal(1);
+        expect(data.h[1]).to.equal(2);
+        expect(data.h[2]).to.equal(3);
+        expect(data.h[3]).to.equal(4);
+        expect(data.i[0]).to.equal('');
+        expect(data.i[1]).to.equal('hello');
+        expect(data.i[2]).to.equal(1);
+        expect(data.l.x).to.equal(0);
+        expect(data.l.y).to.equal(0);
+        expect(data.l.h).to.equal(0);
+        expect(data.l.w).to.equal(0);
+        expect(data.l.k).to.equal(0);
+        expect(data.m).to.equal('');
+        expect(data.fun).to.be.a('function');
+        done();
+      });
+    });
+
+    it('should not modify map object', function(done) {
+      var map = {};
+      ftjs().extractMaps(map, function(data) {
+        expect(data).to.not.equal(map);
+        done();
+      });
+    });
+
+  });
+
+});
