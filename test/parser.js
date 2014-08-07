3aa68b03c85d63248cab53c5d211b2219dff0425
Federico Gandellini <fgandellini@gmail.com>
2014-08-07 10:23:27 +0200

    parser stub and initial test

---
+++ test/parser.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var expect = require('chai').expect;
+var parser = require('../lib/parser');
+
+describe('parser', function() {
+
+  it('should exist', function() {
+    expect(parser).to.exist;
+  });
+
+});
